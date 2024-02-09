074b0d23042bdc156d82b10c5709440a94c6b333
vinceye <vinceye.cm@gmail.com>
2024-02-09 15:56:14 -0800

    removing some boilerplate and outlining skeleton frontend

--- src/app/api/page.tsx
+++ src/app/api/page.tsx
@@ -1,3 +1,11 @@
+import Link from 'next/link';
+import styles from '../page.module.css'
+
 export default function Test() {
-	return <h1>Hello, this is a Test</h1>;
+	return (
+		<main className={styles.main}>
+			<h2>Showing currently implemented API calls:</h2>
+			<Link href='/' className={styles.card}>Back to Home</Link>
+		</main>
+	);
 }
