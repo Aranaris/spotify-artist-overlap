231ba8a6f8a029c8e367977cc82950964e994b60
vinceye <vinceye.cm@gmail.com>
2024-02-16 14:41:11 -0800

    adding changes to get token on button click and some styles

--- src/app/demo-api/page.tsx
+++ src/app/demo-api/page.tsx
@@ -2,23 +2,24 @@
 
 import Link from 'next/link';
 import styles from '../page.module.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function Test() {
 
 	const [validToken, setValidToken] = useState({});
 
-	useEffect(() => {
+	function handleClick() {
 		fetch('/api/spotify')
 			.then((res) => res.json())
 			.then((data)=> {
 				setValidToken(data['authData'])
 			})
-	},[])
+	}
 
 	return (
 		<main className={styles.main}>
 			<h2>Showing currently implemented API calls:</h2>
+			<button className={styles.button} onClick={handleClick}>Test Get Bearer Token</button>
 			<p>{JSON.stringify(validToken)}</p>
 			<Link href='/' className={styles.card}>Back to Home</Link>
 		</main>
