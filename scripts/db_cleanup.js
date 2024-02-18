307dec1f613eb1912ac05c8e86d7893a16479774
vinceye <vinceye.cm@gmail.com>
2024-02-17 22:34:55 -0800

    updated script to actually delete tokens

--- scripts/db_cleanup.js
+++ scripts/db_cleanup.js
@@ -7,10 +7,8 @@ const testClient = new MongoClient(
 
 async function run() {
 	await testClient.connect();
-	const result = testClient.db('spotify_web_app').collection('tokens').find({});
-	for await (const doc of result) {
-		console.log(doc)
-	}
+	await testClient.db('spotify_web_app')
+		.collection('tokens').deleteMany({});
 	await testClient.close();
 }
 
