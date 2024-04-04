447558f34064e48ac7dcc7ea2f361d2dd5eb388e
vinceye <vinceye.cm@gmail.com>
2024-04-04 12:14:45 -0700

    adding explore page

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,34 +1,14 @@
 'use client';
 
-import {useEffect, useState} from 'react';
-import styles from '../../page.module.css';
-import Link from 'next/link';
+import {useState} from 'react';
+import styles from '../page.module.css';
 import Image from 'next/image';
 import {Artist} from '@/app/_lib/spotify';
 
-export default function Profile({params}: { params: { id: string } }) {
+export default function Explore() {
 
-	const currentUser = params.id;
-	const [userInfo, setUserInfo] = useState({
-		display_name:'',
-		link:'',
-		image_url:'',
-	});
-
-	const [sessionPayload, setSessionPayload] = useState({});
-	const [userTopArtists, setUserTopArtists] = useState<Artist[]>([]);
+	const [relatedArtists, setRelatedArtists] = useState<Artist[]>([]);
 	const [overlapData, setOverlapData] = useState<Array<OverlapAggregation>>([]);
-
-	useEffect(() => {
-		fetch(`/api/mongodb/user/${currentUser}`)
-			.then((res) => res.json())
-			.then(setUserInfo);
-	}, [currentUser]);
-
-	function handleGetSessionPayload() {
-		fetch('/api/spotify/auth')
-			.then(res => res.json()).then(setSessionPayload);
-	}
 
 	function handleGetUserTopArtists() {
 		try {
@@ -36,7 +16,7 @@ export default function Profile({params}: { params: { id: string } }) {
 				.then((res) => {
 					if (!res.ok) throw new Error('Failed to retrieve data'); return res.json();
 				}).then((data) => {
-					setUserTopArtists(data);
+					setRelatedArtists(data);
 					setOverlapData(calculateOverlapData(data));
 				});
 
@@ -87,28 +67,13 @@ export default function Profile({params}: { params: { id: string } }) {
 
 	return (
 		<section className={styles.main}>
-			{userInfo['display_name'] !== '' &&
-			<div className={styles['profile-header']}>
-				<Image alt='user profile' className={styles.logo} src={userInfo['image_url']} width={24} height={24}></Image>
-				<h2>{userInfo['display_name']}</h2>
-			</div>
-			}
-			<ul className={styles['info-list']}>
-				<li>Display Name: {userInfo['display_name']}</li>
-				<li>Spotify API Link: <a href={userInfo['link']}>{userInfo['link']}</a></li>
-			</ul>
-
 			<button className={styles.button} onClick={handleGetUserTopArtists}>View My Top Artists</button>
 			<ol className={styles['artist-list']}>
-				{userTopArtists.map((data: Artist, index) =>
+				{relatedArtists.map((data: Artist, index) =>
 					<li key={data['id']}>
 						<p>{index + 1}.</p>
 						<Image alt='artist image' className={styles.logo} src={data['images'][0]['url']} width={24} height={24}></Image>
 						<p>{data['name']}</p>
-						<div className={styles['related-artist-list']}><p><em>Related Artists: </em></p>{data['related_artists'].map((artist) =>
-							<p key={data['id'] + artist['id']}>| {artist['name']} |</p>,
-						)}
-						</div>
 					</li>,
 				)}
 			</ol>
@@ -129,9 +94,6 @@ export default function Profile({params}: { params: { id: string } }) {
 					</li>,
 				)}
 			</ol>
-			<button className={styles.button} onClick={handleGetSessionPayload}>Show Session Payload</button>
-			<p>{JSON.stringify(sessionPayload)}</p>
-			<Link href='/' className={styles.card}>Back to Home</Link>
 		</section>
 	);
 }
